2e1d0b3f01bb9b560d03dfcbbe61e06d2e31023c
Carmen <carmencita.mb9@gmail.com>
2021-09-21 20:44:40 -0300

    first commit

---
+++ M1_E1_P1.py
@@ -0,0 +1,24 @@
+
+""""**********************************
+Autor       : Bristela Muñoz Burgos
+Carrera     : Ingeniería en Informática
+Ramo        : Programación Básica
+Modulo      : M1-E1
+Ejercicio 1 :  Confecciona un programa que reciba la temperatura en grados Celsius, y la traspase a grados
+**********************************
+"""
+# Confecciona un programa que reciba la temperatura en grados Celsius, y la traspase a grados
+# Fahrenheit: \
+
+celcius = input("Ingrese Grados Celcius: ")
+ # print("El valor ingresado es: ")
+Fahrenheit = (int (celcius) *int (9 / 5)) + 32
+print("Los grados celcius en Grados Fahrenheit es:", Fahrenheit)
+
+#grdC = input(msgInput)
+#print("Grados " + grdC + "°C: ")
+
+#Fahrenheit = (int(grdC) * int(9 / 5)) + 32
+#print(msgPrint, Fahrenheit)
+
+
---
+++ M1_E1_P2.py
@@ -0,0 +1,28 @@
+""""**********************************
+Autor       : Bristela Muñoz Burgos
+Carrera     : Ingeniería en Informática
+Ramo        : Programación Básica
+Modulo      : M1-E1
+Ejercicio 2 : Realiza un programa que reciba tres números por teclado y calcule su promedio. 
+**********************************
+"""
+
+print()
+print()
+print("Realiza un programa que reciba tres números por teclado y calcule su promedio")
+print()
+num1= input("ingrese num1:")
+num1= int(num1)
+num2= input("ingrese num2:")
+num2= int(num2)
+
+num3= input("ingrese num3:")
+num3= int(num3)
+
+promedio= (num1+num2+num3)/3
+print("el promedio de notas es:" , promedio)
+
+
+
+
+
